ba81870c564527d6e4aee4ef3f5116a07e72b585
Volodymyr Aleshchenko <v0v04ka.av@gmail.com>
2021-09-02 00:16:14 +0300

    tasks 6 and 8 completed

--- src/es-features/task6.js
+++ src/es-features/task6.js
@@ -28,4 +28,25 @@ export function task6Old() {
 // Напишите реализацию функции task6Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task6New() {}
+export function task6New() {
+  function userModule() {
+    return {
+      name: 'Max',
+      value: 100,
+      role: { name: 'Admin' },
+      cases: [{ id: '1' }],
+    };
+  }
+
+  const tmp = userModule();
+  const {
+    name,
+    value,
+    role: { name: role },
+    isActive = false,
+    cases,
+  } = tmp;
+  const firstCaseId = tmp.cases[0].id;
+
+  return [name, value, role, isActive, firstCaseId, cases];
+}
